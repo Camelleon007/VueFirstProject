ae1bc423a32ff066c5ebbc887b6ae2c273ee313d
Kamil Kowalski <kamil@olicom.pl>
2021-10-10 23:39:45 +0200

    Just formatting

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-new Vue ({
-    el: "#app",
-    data() {
-        return{
-            message: 'Hello Vue!',
-            goals: [],
-            enteredValue: ''
-        };
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      message: "Hello Vue!",
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
     },
-    methods: {
-        addGoal() {
-            this.goals.push(this.enteredValue);
-            this.enteredValue = '';
-        }
-    }
+  },
 });
-
