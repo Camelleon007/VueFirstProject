a6d47d57368961aca79cd8c758f198604b8e2358
Kamil Kowalski <kamil@olicom.pl>
2021-10-11 23:22:00 +0200

    Method

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const app = Vue.createApp({
   data() {
     return {
-      title: "WakeCity",
+      title: "Wake park: ",
+      titleA: "WakeCity",
+      titleB: "CWP",
       description: "My home spot.",
-      link: "http://wakecity.pl"
+      link: "http://wakecity.pl",
     };
-  }
+  },
+  methods: {
+    setTitle() {
+      const randomNumber = Math.random();
+      if (randomNumber > 0.5) {
+        return this.titleA;
+      } else {
+        return this.titleB;
+      }
+    },
+  },
 });
 
 app.mount("#wake-park");
